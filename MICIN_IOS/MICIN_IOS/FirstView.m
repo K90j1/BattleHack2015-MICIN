e1c81a3b16f5efd5540f07112ee37a943918a036
bookair17 <kyoji-exterior@goo.jp>
2015-06-16 09:08:33 +0900

    Update doc

--- MICIN_IOS/MICIN_IOS/FirstView.m
+++ MICIN_IOS/MICIN_IOS/FirstView.m
@@ -1,6 +1,6 @@
 //
-// Created by bookair17 on 6/14/15.
-// Copyright (c) 2015 bookair17. All rights reserved.
+// Apache License, Version 2.0
+// Copyright (c) 2015 routeflags. All rights reserved.
 //
 
 #import "FirstView.h"
